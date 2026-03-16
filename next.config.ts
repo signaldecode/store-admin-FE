bf7d5b2562f026927e445be799b09ed099a63809
YHpark222 <parkyoohyun@signal-decode.com>
2026-03-16 18:59:51 +0900

    fix : global-err 삭제

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
   reactCompiler: true,
-  experimental: {
-    /** Next.js 16 global-error prerender 버그 우회 */
-    staticGenerationRetryCount: 0,
-  },
 };
 
 export default nextConfig;
